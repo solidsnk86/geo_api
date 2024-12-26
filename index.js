674727e74d6dbda222fe3dcc45556375a2f249ae
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-12-26 02:09:04 -0300

    fix img

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ app.get('/', async (req, res) => {
       country: {
         name: country,
         flag: `<img src="https://flagcdn.com/16x12/${country?.toLowerCase()}.png" 
-                srcset="https://flagcdn.com/32x24/${country?.toLowerCase()}.png 2x,  
-                https://flagcdn.com/48x36/${country?.toLowerCase()}.png 3x"
                 width="16"
                 height="12"
                 alt="Argentina">`,
