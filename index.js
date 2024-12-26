995cbc97484cf17ff92e65a8eaec6dd0ee230adc
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-12-26 02:29:07 -0300

    feat: flags sizes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import getNetworkInterfaces from './services/get-network-interfaces.js'
 import dotenv from 'dotenv'
-import moment from 'moment-timezone'
 
 const app = express()
 
@@ -31,7 +30,11 @@ app.get('/', async (req, res) => {
       },
       country: {
         name: country,
-        flag: `<img src='https://flagcdn.com/16x12/${country?.toLowerCase()}.png' alt='${country}' width='45' height='45' />`,
+        flag: {
+          size_1: `https://flagcdn.com/16x12/${country?.toLowerCase()}.png`,
+          size_2: `https://flagcdn.com/32x34/${country?.toLowerCase()}.png`,
+          size_3: `https://flagcdn.com/48x36/${country?.toLowerCase()}.png`
+        },
         time_zone: timeZone,
       },
       network_interfaces: getNetworkInterfaces(),
@@ -129,7 +132,7 @@ app.get('/', async (req, res) => {
         <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
       </svg>
     </a>
-    <img src='https://flagcdn.com/16x12/${country?.toLowerCase()}.png' alt='${country}' width='45' height='45' />
+    <img src='https://flagcdn.com/32x34/${country?.toLowerCase()}.png' alt='${country}' width='45' height='45' />
     <script type="module">
       import {
         highlight
