c2c4460f6f2ae07f9540ac42967e9feb1417936e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-11 20:37:37 -0300

    feat: updated card effect in footer

--- views/main-view.js
+++ views/main-view.js
@@ -13,72 +13,113 @@ export const mainView = ({ data }) => `
       code.innerHTML = highlight(code.innerText);
     </script>
     <style>
+    :root {
+      --color: #000000;
+      --background-color: #ffffff;
+      --sh-class: #000000;
+      --sh-identifier: #000000;
+      --sh-sign: rgba(0, 0, 0, 0.5);
+      --sh-string: #000000;
+      --sh-keyword: #000000;
+      --sh-comment: #000000;
+      --sh-jsxliterals: #000000;
+    }
+    @media (prefers-color-scheme: dark) {
       :root {
-        --color: #000000;
-        --background-color: #ffffff;
-        --sh-class: #000000;
-        --sh-identifier: #000000;
-        --sh-sign: rgba(0, 0, 0, 0.5);
-        --sh-string: #000000;
-        --sh-keyword: #000000;
-        --sh-comment: #000000;
-        --sh-jsxliterals: #000000;
+        --color: #ffffff;
+        --background-color: #000000;
+        --sh-class: #ffffff;
+        --sh-identifier: #ffffff;
+        --sh-sign: rgba(255, 255, 255, 0.5);
+        --sh-string: #ffffff;
+        --sh-keyword: #ffffff;
+        --sh-comment: #ffffff;
+        --sh-jsxliterals: #ffffff;
       }
-
-      @media (prefers-color-scheme: dark) {
-        :root {
-          --color: #ffffff;
-          --background-color: #000000;
-          --sh-class: #ffffff;
-          --sh-identifier: #ffffff;
-          --sh-sign: rgba(255, 255, 255, 0.5);
-          --sh-string: #ffffff;
-          --sh-keyword: #ffffff;
-          --sh-comment: #ffffff;
-          --sh-jsxliterals: #ffffff;
-        }
+    }
+    body {
+      background: var(--background-color);
+    }
+    code {
+      font-size: 2vmin;
+      font-family: 'Operator Mono', 'Fira Code', 'SF Mono', 'Roboto Mono', Menlo,
+        monospace;
+      line-height: 1.5;
+    }
+    section {
+      position: fixed;
+      bottom: 10px;
+      right: 10px;
+    }
+    .card {
+      margin: 0 auto;
+      padding: .5em;
+      width: fit-content;
+      background: #000;
+      text-align: center;
+      border-radius: 8px;
+      position: relative;
+    }
+    @property --angle {
+      syntax: '<angle>';
+      initial-value: 0deg;
+      inherits: false;
+    }
+    .card::after,
+    .card::before {
+      content: '';
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background: conic-gradient(
+        from var(--angle),
+        #ff4545,
+        #00ff99,
+        #006aff,
+        #ff0095,
+        #ff4545
+      );
+      top: 50%;
+      left: 50%;
+      translate: -50% -50%;
+      z-index: -1;
+      padding: 1px;
+      border-radius: 8px;
+      animation: 3.5s spin linear infinite;
+    }
+    .card::before {
+      filter: blur(1.5rem);
+      opacity: 0.5;
+      animation: 3s spin linear infinite;
+    }
+    @keyframes spin {
+      to {
+        --angle: 360deg;
       }
-
-      body {
-        background: var(--background-color);
-      }
-
-      code {
-        font-size: 2vmin;
-        font-family: "Operator Mono", "Fira Code", "SF Mono", "Roboto Mono", Menlo,
-          monospace;
-        line-height: 1.5;
-      }
-
-      .solid {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        text-decoration: none;
-        color: var(--color);
-        font-size: 2vmin;
-        font-family: "Operator Mono", "Fira Code", "SF Mono", "Roboto Mono", Menlo,
-          monospace; 
-        display: flex;
-        width: fit-content;
-        align-items: center;
-        gap: 4px;
-      }
-
-      .solid:hover img {
-        filter: drop-shadow(0 0 20px #0078D7);
-        transition: all 1s ease;
-      }
-    </style>
+    }
+    .solid {
+      text-decoration: none;
+      color: var(--color);
+      font-size: 2vmin;
+      font-family: 'Operator Mono', 'Fira Code', 'SF Mono', 'Roboto Mono', Menlo,
+        monospace;
+      display: flex;
+      width: fit-content;
+      align-items: center;  
+    }
+  </style>
   </head>
   <body>
     <pre>
       <code>${JSON.stringify(data, null, 2)}</code>
     </pre>
-    <a href="https://github.com/solidsnk86/" target="_blank" rel="noopener noreferrer nofollow" class="solid" aria-label="View profile on GitHub">
-      Made with 💙 by 
-      <img src="https://raw.githubusercontent.com/solidsnk86/portfolio-mgc-2024/master/public/solidsnk86.png" alt="Solid Snake PixelArt" style="image-rendering: pixelated;" width="50" height="50" />
-    </a>
+    <section>
+      <div class="card">
+      <a href="https://github.com/solidsnk86/" target="_blank" rel="noopener noreferrer nofollow" class="solid" aria-label="View profile on GitHub">
+      Made with 🤍 by @solidSnk86
+      </a>
+    </div>
+    </section>
   </body>
 </html>
-`
+`;
