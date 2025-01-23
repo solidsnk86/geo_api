8dc7a0b46d676742f811eb0c99cc710bd52b52b8
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-23 07:44:58 -0300

    updated api

--- services/closest-airport.js
+++ services/closest-airport.js
@@ -2,22 +2,18 @@ const EARTH_RADIUS = 6378137
 const square = (num) => num * num
 const degreesToRadians = (degrees) => (degrees * Math.PI) / 180.0
 
-// Calculate Haversine function
 const haversine = (pointA, pointB) => {
-  // Convert latitude and longitude from degrees to radians
   const latitudeA = degreesToRadians(pointA.lat)
   const latitudeB = degreesToRadians(pointB.lat)
   const longitudeA = degreesToRadians(pointA.lon)
   const longitudeB = degreesToRadians(pointB.lon)
 
-  // Haversine formula
   const haversineTheta =
     square(Math.sin((latitudeB - latitudeA) / 2)) +
     Math.cos(latitudeA) *
       Math.cos(latitudeB) *
       square((longitudeB - longitudeA) / 2)
 
-  // Calculate distance using Haversine formula
   const distance = 2 * EARTH_RADIUS * Math.asin(Math.sqrt(haversineTheta))
 
   return distance / 1000
