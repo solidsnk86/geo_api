03154870735e291e8451bc00d4333d439e15cd6a
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-07-03 00:35:15 -0300

    chore: fix typo

--- services/get-cities.js
+++ services/get-cities.js
@@ -1,8 +1,8 @@
 const getAllCitiesAR = async () => {
-  const resposne = await fetch(
+  const response = await fetch(
     'https://cdn.jsdelivr.net/gh/liquidsnk86/cdn-js@main/solid-geodata-v4.json'
   )
-  const jsonData = await resposne.json()
+  const jsonData = await response.json()
   const formatJSON = Object.keys(jsonData).map((key) => {
     const { id, nombre, tipo, departamento, provincia, pais, lat, lon } =
       jsonData[key]
