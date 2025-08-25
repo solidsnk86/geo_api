d24e5958e5910b0c678a13939783964f8cccccc8
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-24 21:22:10 -0300

    updated new route

--- controller/QrCodeController.js
+++ controller/QrCodeController.js
@@ -24,4 +24,23 @@ export class QrCodeController {
       res.status(500).send('Error al generar el código QR')
     }
   }
+
+  static async generateDataURL(req, res) {
+    const { text } = req.query
+    if (!text) {
+      return res.status(400).json({ message: 'Falta parámetro en la url' })
+    }
+    try {
+      const dataURL = QRCode.toDataURL(text, {
+        type: 'image/png',
+        width: 800,
+        margin: 2,
+        errorCorrectionLevel: 'H',
+      })
+      const dataBaseURL = await dataURL
+      res.status(200).json({ dataBaseURL })
+    } catch (error) {
+      res.status(500).json({ error: 'Error al generar el buffer: ' + error })
+    }
+  }
 }
