ca8668a90ed367a88b9cfd7c0c2822f7ca82caa9
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-06-21 17:31:12 -0300

    *-*-

--- controller/LoggerController.js
+++ controller/LoggerController.js
@@ -2,8 +2,12 @@ import { request, response } from 'express'
 
 export class LogerController {
   collectData = async (req = request, res = response) => {
+    const ip = req.ip
+    const { headers, host, hostname, readableHighWaterMark, originalUrl } = req
     try {
-      return res.status(200).json({ ip: req.ip, req })
+      return res
+        .status(200)
+        .json({ ip, host, hostname, readableHighWaterMark, originalUrl })
     } catch (error) {
       return res.json({ message: 'Error', error: error.message })
     }
