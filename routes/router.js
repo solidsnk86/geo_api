4a910b97279d0e9870529d2a7ffaef63ae0b34c4
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-06-21 17:58:13 -0300

    remove route

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,8 @@
 import { Router } from 'express'
 import { GeoController } from '../controller/GeoController.js'
 import { QrCodeController } from '../controller/QrCodeController.js'
-import { LogerController } from '../controller/LoggerController.js'
 
 export const geoRouter = Router()
-const loggerController = new LogerController();
 
 geoRouter.get('/', GeoController.home)
 geoRouter.get('/docs', GeoController.docs)
@@ -12,4 +10,3 @@ geoRouter.get('/location', GeoController.location)
 geoRouter.get('/geolocation', GeoController.geolocation)
 geoRouter.get('/qr', QrCodeController.generateQrCode)
 geoRouter.get('/qr/buffer', QrCodeController.generateDataURL)
-geoRouter.get('/request', loggerController.collectData)
